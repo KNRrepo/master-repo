b6a850669c89d9e1d3d741cf5ae3b8cf45d8d7cd
KNRrepo <101704281+KNRrepo@users.noreply.github.com>
2022-03-22 19:15:30 +0530

    Add files via upload

---
+++ Careers.js
@@ -0,0 +1,44 @@
+import withRoot from './modules/withRoot';
+// --- Post bootstrap -----
+import React from 'react';
+
+
+
+
+import { Link } from 'react-router-dom';
+
+
+
+function Careers() {
+    return (
+        <div >
+        <img
+            width={600} height={150}
+            src="https://carnamedicare.com/wp-content/uploads/2020/07/Career-Image.png"
+            alt="CARRERS"
+          />
+          <br/>
+          <a><a></a>
+          <br/>
+          <br/>
+          <br/>
+          <br/>
+          <br/>
+          <br/>
+          <h4>" Are you inspired to make a difference in the field of education? And looking for higher prospects in teaching? Mail your resume at info@npsypr.com or post it to our address. Resume should be accompanied with handwritten write up on " What strategies will you employ to make your subject interesting to students " or "How will you contribute to the child's holistic development" and recent passport size coloured photograph."
+    <br></br>
+    </h4></a>
+          <hr
+        style={{
+          backgroundColor: 'black',
+          height: 2,
+          width: 900
+        }}
+      />
+    
+         
+        </div>
+      );
+    }
+
+export default Careers;
